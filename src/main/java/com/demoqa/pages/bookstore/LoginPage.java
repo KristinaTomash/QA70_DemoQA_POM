9bc512cfef2d1569f874541a50797847b781fe9e
KristinaTomash <kristitomsh@gmail.com>
2025-12-01 13:09:41 +0200

    Add 2

---
+++ src/main/java/com/demoqa/pages/bookstore/LoginPage.java
@@ -0,0 +1,30 @@
+package com.demoqa.pages.bookstore;
+
+import com.demoqa.core.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+        @FindBy(id = "userName")
+    WebElement userNameInput;
+
+        @FindBy(id = "password")
+    WebElement passwordInput;
+    public LoginPage enterUserData(String name, String password) {
+        type(userNameInput,name);
+        //typeWithJS(userNameInput,name,0,300);
+        type(passwordInput,password);
+        return this;
+    }
+@FindBy(id = "login")
+WebElement loginButton;
+    public ProfilePage clickOnLoginButton() {
+        click(loginButton);
+
+        return new ProfilePage(driver);
+    }
+}
---
+++ src/test/java/com/demoqa/tests/BookStoreTests.java
@@ -0,0 +1,23 @@
+package com.demoqa.tests;
+
+import com.demoqa.core.TestBase;
+import com.demoqa.pages.HomePage;
+import com.demoqa.pages.SidePanel;
+import com.demoqa.pages.bookstore.LoginPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BookStoreTests extends TestBase {
+
+    @BeforeEach
+    public void precondition(){
+        new HomePage(driver).selectBookStore();
+    }
+    @Test
+    public void loginPositiveTest(){
+        new SidePanel(driver).selectLogin();
+        new LoginPage(driver).enterUserData("Kris","Kk1234567$")
+                .clickOnLoginButton()
+                .verifyUserName("Kris");
+    }
+}
